0777ad6e01e5fce98df3e35dfe00b8ef2c56396a
exseo <jarredmadero@gmail.com>
2018-02-05 12:48:31 -0800

    First commit of barebones.

--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -16,5 +16,10 @@ namespace SuperAdventure
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
