7e998316962348f6ccb15477a2ece2f52e5ad211
Exseo <Jarredmadero@gmail.com>
2018-02-21 15:09:36 -0800

    Merge remote into master to a playable state. (#1)
    Merge current progress in remote into master. This updates master to a playable state without combat for logic or using items. Doesn't crash though!

---
+++ Engine/LivingCreature.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Engine
+{
+    public class LivingCreature
+    {
+        //properties to generate for living creatures
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
+
+        public LivingCreature(int currentHitPoints, int maximumHitPoints)
+        {
+            CurrentHitPoints = currentHitPoints;
+            MaximumHitPoints = maximumHitPoints;
+        }
+    
+    }
+}
---
+++ Engine/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Engine
+{
+    public class Weapon:Item //semicolon indicates the base class
+    {
+        //properties to generate for weapons
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
+
+        public Weapon(int id, string name, string namePlural, int minimumDamage, int maximumDamge): base(id, name, namePlural)
+        {
+            MinimumDamage = minimumDamage;
+            MaximumDamage = MaximumDamage;
+        }
+    }
+}
